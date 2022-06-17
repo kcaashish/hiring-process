1829f503354cd7d8f8b89c9862c542d08341858b
Aashish KC <kcaashish@gmail.com>
2022-06-17 14:06:11 +0545

    descriptive return from models jobs and candidates

--- candidates/models.py
+++ candidates/models.py
@@ -4,7 +4,13 @@ class Candidate(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class CandidateSkills(models.Model):
     candidate_id = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     skills_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.skills_name
--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Jobs(models.Model):
     job_title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.job_title
+
 class JobSkills(models.Model):
     job_id = models.ForeignKey(Jobs, on_delete=models.CASCADE)
     skills_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.skills_name
